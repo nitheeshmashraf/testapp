67b6eac2a7f261fcb25f53e7571b33528a31de57
Nitheesh <nitheeshmashraf@gmail.com>
2017-10-08 17:35:58 +0400

    mongo db

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,39 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-// declare axios for making http requests
-const axios = require('axios');
-const API = 'https://jsonplaceholder.typicode.com';
+// Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+        if (err) return console.log(err);
 
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('api works');
-});
+        closure(db);
+    });
+};
 
-// Get all posts
-// router.get('/posts', (req, res) => {
-//   // Get posts from the mock api
-//   // This should ideally be replaced with a service that connects to MongoDB
-//   axios.get(`${API}/posts`)
-//     .then(posts => {
-//       res.status(200).json(posts.data);
-//     })
-//     .catch(error => {
-//       res.status(500).send(error)
-//     });
-// });
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
 
-router.get('/posts/1', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts/1`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+    connection((db) => {
+        db.collection('users')
+            .find()
+            .toArray()
+            .then((users) => {
+                response.data = users;
+                res.json(response);
+            })
+            .catch((err) => {
+                sendError(err, res);
+            });
     });
 });
-
 
 module.exports = router;
